31b022605beb63aa6e4246302150506fe99e2843
Adam Presley <adam@adampresley.com>
2014-04-30 23:10:10 -0500

    Sorting now works on the table. Also changed the layout plugin to one that works better. Also moved the Ractive templates out of the HTML into separate partial files. closes #3

--- www/resources/js/app/index.js
+++ www/resources/js/app/index.js
@@ -1,11 +1,18 @@
-require(["./resources/js/config"], function() {
+//require(["./resources/js/config"], function() {
 	require(
 		[
+			/* Injected dependencies */
 			"jquery", "app/MailSlurper", "rajo.pubsub", "rajo.ui.bootstrapmodal", "Ractive",
 			"app/MailCollection",
+
+			/* Templates */
+			"text!/resources/templates/mail-list.html",
+			"text!/resources/templates/mail-view.html",
+
+			/* Other non-injected dependencies */
 			"layout"
 		],
-		function($, MailSlurper, PubSub, BootstrapModal, Ractive, MailCollection) {
+		function($, MailSlurper, PubSub, BootstrapModal, Ractive, MailCollection, MailListPartial, MailViewPartial) {
 			"use strict";
 
 			PubSub.publish("mailslurper.block", "Loading mails...");
@@ -21,31 +28,78 @@ require(["./resources/js/config"], function() {
 			var
 				mails = [],
 
+				/*
+				 * Ractive instance to handle the list of mail items
+				 */
 				mailListRactive = new Ractive({
 					el: "mailList",
-					template: "#mailListTemplate",
+					//template: "#mailListTemplate",
+					template: "{{>mailList}}",
+					partials: {
+						mailList: MailListPartial
+					},
 					data: {
 						mails: mails,
 						sortColumn: "dateSent",
+						sortDirection: "desc",
 
 						compressTo: function(toAddresses) {
 							return toAddresses.join("; ");
 						},
 
+						/*
+						 * Called when clicking on a header column to sort.
+						 * This method will sort the array of data based on a passed
+						 * in column and current sort order.
+						 *
+						 * There is an event attached to this Ractive instance
+						 * that will swap the current sort direction.
+						 */
 						sort: function(array, column) {
-							console.log("in sort %o", arguments);
+							var
+								dir = this.get("sortDirection"),
+								result1 = (dir === "desc") ? 1 : -1,
+								result2 = (dir === "desc") ? -1 : 1;
+
 							array = array.slice();
 
 							return array.sort(function(a, b) {
-								return a[column] < b[column] ? -1 : 1;
+								return a[column] < b[column] ? result1 : result2;
 							});
+						},
+
+						/*
+						 * Returns the correct CSS classes for a column
+						 * based on if it is the current sort column and
+						 * what the direction is.
+						 */
+						getSortIcon: function(column) {
+							var
+								sc = this.get("sortColumn"),
+								sd = this.get("sortDirection");
+
+							if (sc !== column) {
+								return "";
+							} else {
+								if (sd === "desc") {
+									return "glyphicon glyphicon-arrow-down";
+								} else {
+									return "glyphicon glyphicon-arrow-up";
+								}
+							}
 						}
 					}
 				}),
 
+				/*
+				 * Ractive to handle viewing a single mail item's details
+				 */
 				mailViewRactive = new Ractive({
 					el: "mailView",
-					template: "#mailViewTemplate",
+					template: "{{>mailView}}",
+					partials: {
+						mailView: MailViewPartial
+					},
 					data: {
 						mailView: "",
 						subject: "",
@@ -92,6 +146,12 @@ require(["./resources/js/config"], function() {
 				},
 
 				sort: function(e, column) {
+					if (this.get("sortColumn") === column) {
+						this.set("sortDirection", (this.get("sortDirection") === "desc") ? "asc" : "desc");
+					} else {
+						this.set("sortDirection", "desc");
+					}
+
 					this.set("sortColumn", column);
 				}
 			});
@@ -109,4 +169,4 @@ require(["./resources/js/config"], function() {
 			setupWebsocket();
 		}
 	);
-});
+//});
--- www/resources/js/config.js
+++ www/resources/js/config.js
@@ -2,6 +2,8 @@ require.config({
 	baseUrl: "resources/js",
 	paths: {
 		"app": "app",
+		"text": "requirejs/text",
+
 		"persona": "//login.persona.org/include.js",
 		"jqueryui": "jquery/jquery-ui-1.10.4.min",
 		"bootstrap": "jquery/bootstrap",
@@ -16,12 +18,6 @@ require.config({
 		"Ractive": "jquery/Ractive",
 		"daterangepicker": "jquery/daterangepicker",
 		"moment": "jquery/moment",
-		"jlayout.border": "jquery/jlayout.border",
-		"jlayout.flexgrid": "jquery/jlayout.flexgrid",
-		"jlayout.flow": "jquery/jlayout.flow",
-		"jlayout.grid": "jquery/jlayout.grid",
-		"jquery.jlayout": "jquery/jquery.jlayout",
-		"jquery.sizes": "jquery/jquery.sizes",
 
 		"layout": "jquery/jquery.layout"
 	},
@@ -30,13 +26,6 @@ require.config({
 		"jquery.blockUI": { deps: ["jquery"] },
 		"bootstrap": { deps: ["jquery"] },
 		"daterangepicker": { deps: ["moment", "bootstrap"] },
-		"jquery.sizes": { deps: ["jquery"] },
-		"jquery.jlayout": { deps: ["jquery", "jlayout.border"] },
-		"jlayout.border": { deps: ["jquery.sizes"] },
-		"jlayout.flexgrid": { deps: ["jquery.sizes"] },
-		"jlayout.flow": { deps: ["jquery.sizes"] },
-		"jlayout.grid": { deps: ["jquery.sizes"] },
-
 		"layout": { deps: ["jqueryui"] }
 	}
 });
