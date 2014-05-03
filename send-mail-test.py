748b2c62eb12d6bad422432d886a29db4807dec1
Adam Presley <adam@adampresley.com>
2014-05-02 21:49:45 -0500

    Mail generator tool for testing now pulls quotes from iheartquotes.com. Made test mails so much better. refs #10

--- send-mail-test.py
+++ send-mail-test.py
@@ -1,40 +1,59 @@
 #
 # Use this script to quickly send a bunch of mails. Useful for testing.
 #
-import smtplib
-import datetime
+import sys
+import json
 import time
+import smtplib
+import urllib2
+import datetime
+
 from email.mime.text import MIMEText
 
-numMails = 10
-address = "127.0.0.1"
-smtpPort = 8000
+def getQuote():
+	response = urllib2.urlopen("http://www.iheartquotes.com/api/v1/random?format=json")
+	obj = json.loads(response.read())
 
-me = "someone@another.com"
-to = "bob@bobtestingmailslurper.com"
-body = """Hello,
+	quoteLines = obj["quote"].split("--")
 
-This is a test message. It is here to test the SMTP server and
-admin called MailSlurper. I sure hope it all works!!
+	if len(quoteLines) > 0:
+		return {
+			"quote": quoteLines[0].strip(),
+			"source": "Unknown" if len(quoteLines) <= 1 else quoteLines[1].strip(),
+		}
+	else:
+		return {
+			"quote": "No quote",
+			"source": "Adam Presley"
+		}
 
-Sincerely,
-Adam Presley"""
+if __name__ == "__main__":
+	numMails = 5
+	address = "127.0.0.1"
+	smtpPort = 8000
 
-try:
+	me = "someone@another.com"
+	to = "bob@bobtestingmailslurper.com"
 
-	for index in range(numMails):
-		msg = MIMEText(body)
-		msg["Subject"] = "Test Message #{0}".format(index)
-		msg["From"] = me
-		msg["To"] = to
-		msg["Date"] = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S +0000 UTC")
+	try:
+		for index in range(numMails):
+			quote = getQuote()
 
-		server = smtplib.SMTP("{0}:{1}".format(address, smtpPort))
-		server.sendmail(me, [to], msg.as_string())
-		server.quit()
+			body = "<p>Hello,</p><p>Here is today's quote.</p><p><em>{0}</em><br />&nbsp;&nbsp;-- {1}</p><p>Sincerely,<br />Adam Presley</p>".format(quote["quote"], quote["source"],)
+			msg = MIMEText(body)
 
-		time.sleep(1)
+			msg["Subject"] = "Quote From {0}".format(quote["source"])
+			msg["From"] = me
+			msg["To"] = to
+			msg["Date"] = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S +0000 UTC")
+
+			server = smtplib.SMTP("{0}:{1}".format(address, smtpPort))
+			server.sendmail(me, [to], msg.as_string())
+			server.quit()
+
+			time.sleep(2)
 
 
-except Exception as e:
-	print("An error occurred while trying to connect and send the email: {0}".format(e.message))
+	except Exception as e:
+		print("An error occurred while trying to connect and send the email: {0}".format(e.message))
+		print(sys.exc_info())
