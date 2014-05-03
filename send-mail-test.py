c92ea8aa2285de0df756e3ee5b4bcb97dcec4583
Adam Presley <adam@adampresley.com>
2014-05-02 22:38:35 -0500

    Mail send test now sends multipart mails refs #10

--- send-mail-test.py
+++ send-mail-test.py
@@ -9,6 +9,7 @@ import urllib2
 import datetime
 
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 def getQuote():
 	response = urllib2.urlopen("http://www.iheartquotes.com/api/v1/random?format=json")
@@ -39,13 +40,21 @@ if __name__ == "__main__":
 		for index in range(numMails):
 			quote = getQuote()
 
-			body = "<p>Hello,</p><p>Here is today's quote.</p><p><em>{0}</em><br />&nbsp;&nbsp;-- {1}</p><p>Sincerely,<br />Adam Presley</p>".format(quote["quote"], quote["source"],)
-			msg = MIMEText(body)
+			textBody = "Hello,\nHere is today's quote.\n\n{0}\n  -- {1}\n\nSincerely,\nAdam Presley".format(quote["quote"], quote["source"])
+			htmlBody = "<p>Hello,</p><p>Here is today's quote.</p><p><em>{0}</em><br />&nbsp;&nbsp;-- {1}</p><p>Sincerely,<br />Adam Presley</p>".format(quote["quote"], quote["source"],)
+
+			text = MIMEText(textBody, "plain")
+			html = MIMEText(htmlBody, "html")
+
+			msg = MIMEMultipart("alternative")
 
 			msg["Subject"] = "Quote From {0}".format(quote["source"])
 			msg["From"] = me
 			msg["To"] = to
 			msg["Date"] = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S +0000 UTC")
+
+			msg.attach(text)
+			msg.attach(html)
 
 			server = smtplib.SMTP("{0}:{1}".format(address, smtpPort))
 			server.sendmail(me, [to], msg.as_string())
