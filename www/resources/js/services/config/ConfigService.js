0f4e1d36d986fdbd9964345c86d1582ccb310eff
Adam Presley <adam@adampresley.com>
2014-05-02 19:14:37 -0500

    Moved and refactored ConfigService refs #11

---
+++ www/resources/js/services/config/ConfigService.js
@@ -0,0 +1,40 @@
+// Copyright 2013 Adam Presley. All rights reserved
+// Use of this source code is governed by the MIT license
+// that can be found in the LICENSE file.
+
+/**
+ * Class: ConfigService
+ * Defines a service for working with config data
+ *
+ * Author:
+ *     Adam Presley
+ *
+ * Endpoint:
+ *    /config
+ *
+ * Dependencies:
+ *    * <Http>
+ */
+define(
+	[
+		"modules/util/Http"
+	],
+	function(Http) {
+		var endpoint = "/config";
+
+		return {
+			get: function() {
+				return Http.get(endpoint);
+			},
+
+			save: function(www, wwwPort, smtpAddress, smtpPort) {
+				return Http.put(endpoint, {
+					www: www,
+					wwwPort: wwwPort,
+					smtpAddress: smtpAddress,
+					smtpPort: smtpPort
+				});
+			}
+		};
+	}
+);
